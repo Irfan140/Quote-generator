806ac1e2f9cdbcf370cb05ca9a989640db4cff01
Irfan140 <irfanmehmud140@gmail.com>
2025-10-01 20:56:01 +0530

    Add LLM to generate quote

--- server/main.py
+++ server/main.py
@@ -1,6 +1,30 @@
 from flask import Flask, jsonify
 import requests
 
+import os
+from dotenv import load_dotenv
+
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+
+llm = ChatGroq(
+    model="deepseek-r1-distill-llama-70b",  
+    temperature=0,                          # Controls randomness (0 = deterministic)
+    max_tokens=None,                        # Maximum tokens in the response (None = default)
+    reasoning_format="parsed",              # Output reasoning in structured (parsed) form
+    timeout=None,                           # How long to wait before request times out (in seconds)
+    max_retries=2,                          # Retry failed requests up to 2 times
+    # other params...                       # You can pass extra kwargs depending on API
+)
+
+
+
+    
+
+
 app = Flask(__name__)
 
 
@@ -11,17 +35,20 @@ def home():
 @app.route("/quote", methods=["GET"])
 def get_quote():
     try:
-        # Call external random quote API
-        response = requests.get("https://api.quotable.io/random", timeout=5, verify=False)
-        data = response.json()
+        prompt_template = PromptTemplate.from_template(
+    "Give me a quote from the anime {anime}. Just Give the sentence of the quote and the author that said it."
+)
 
-        # Extract only what we need
-        quote = {
-            "content": data.get("content", "No quote available"),
-            "author": data.get("author", "Unknown")
-        }
 
-        return jsonify(quote), 200
+        chain = prompt_template | llm
+
+        anime_name = input("Enter anime name: ")
+
+        ai_msg = chain.invoke({"anime": f"{anime_name}"})
+
+        print(ai_msg.content, end="\n\n\n")
+
+        return jsonify(ai_msg.content), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
